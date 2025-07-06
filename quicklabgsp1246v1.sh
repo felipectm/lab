06f1c3b4b0d5fc39bcc768475be5b919762855ea
felipectm <felipe_ctm@hotmail.com>
2025-07-05 23:37:21 -0300

    Update quicklabgsp1246v1.sh

--- quicklabgsp1246v1.sh
+++ quicklabgsp1246v1.sh
@@ -64,8 +64,8 @@ if [ "$FIRST_QUERY_STATUS" -ne 0 ]; then
     exit 1
 fi
 
-echo "Waiting for 10 seconds..."
-sleep 10
+echo "Waiting for 15 seconds..."
+sleep 15
 echo "Done waiting. Proceeding to second bq query."
 
 bq query --use_legacy_sql=false \
